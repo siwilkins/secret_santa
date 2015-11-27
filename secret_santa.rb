d6b9ed594799da4ff76b8d59ea288d76ed0c2fca
Simon Wilkins <simonwilkins@dyn176066.shef.ac.uk>
2015-11-27 14:43:13 +0000

    Add parameterisation and emailing

--- secret_santa.rb
+++ secret_santa.rb
@@ -1,7 +1,52 @@
+require 'optparse'
 require 'mail'
 
+OptionParser.new do |opts|
+  opts.banner = "Usage: secret_santa.rb [options]"
+  opts.on("-m", "--mailhost MAILHOST", "Mailhost") do |m|
+    @mailhost = m
+  end
+  opts.on("-f", "--from EMAILADDRESS", "From Email Address") do |e|
+    @from_email = e
+  end
+  opts.on("-d", "--data FILE", "Data File") do |f|
+    @data_file = f
+  end
+  opts.on("-u", "--username USERNAME", "GMail Username") do |u|
+    @username = u
+  end
+  opts.on("-p", "--password PASSWORD", "GMail Password") do |p|
+    @password = p
+  end
+  opts.on("-h", "--domain", "Sending domain") do |d|
+    @domain = d
+  end
+end.parse!
+
+mail_options = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'gmail.com',
+  user_name: @username,
+  password: @password,
+  authentication: 'login',
+  enable_starttls_auto: true,
+  domain: @domain
+}
+
+Mail.defaults do
+  delivery_method :smtp, mail_options
+end
+
+emails = {}
+@family = []
+File.readlines(@data_file).each do |line|
+  name, email = line.split('|')
+  @family << name
+  emails[name] = email
+end
+
 def reset!
-  @family = %w{Kate Jon Jo Janet Mike Sofia Bec Mark Si Hazel}
   @givers = @family.dup
   @receivers = @family.dup
   @buyers = []
@@ -32,13 +77,13 @@ while !@givers.empty?
   end
 end
 
+from_email = @from_email
 @buyers.each do |giver, receiver|
-  puts "#{giver} buys for #{receiver}"
   mail = Mail.new do
-    from 'secret@santa.com'
-    to 'si.wilkins@gmail.com'
+    to emails[giver]
+    from from_email
     subject 'Secret Santa'
-    body "Hello #{giver},\n\nYou will be buying for #{receiver}.\n\nRegards,\nSanta"
+    body "Hello #{giver},\n\nYou will be giving a present to #{receiver}.\n\nBest wishes,\nSanta"
   end
-
+  mail.deliver!
 end
